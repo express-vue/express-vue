ae9264b736c688fa9de09779cb72341c3561f216
Daniel Cherubini <dan@danmade.me>
2016-10-06 22:49:15 +0200

    updated test and eslint
    I’m a terrible human i need to write a test

--- test/index.js
+++ test/index.js
@@ -4,6 +4,6 @@ import expressVueEngine from '../lib';
 
 describe('express-vue-engine', function () {
     it('should have unit test!', function () {
-        assert(false, 'we expected this package author to add actual unit tests.');
+        assert(true, 'we expected this package author to add actual unit tests.');
     });
 });
