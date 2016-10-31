582e6b304eb891a82f4ffbf9953691601c9cfe1f
Daniel Cherubini <daniel@cherubini.casa>
2016-10-31 14:22:14 +0100

    removed last eval

--- dist/utils/string.js
+++ dist/utils/string.js
@@ -3,22 +3,34 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
+exports.scriptToString = undefined;
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _xss = require('xss');
+
+var _xss2 = _interopRequireDefault(_xss);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function scriptToString(script) {
     var string = '';
     for (var member in script) {
         switch (_typeof(script[member])) {
             case 'function':
-                string += member + ': ' + String(script[member]) + ',';
+                if (member === 'data') {
+                    var dataObj = (0, _xss2.default)(JSON.stringify(script[member]()));
+                    string += member + ': function(){return ' + dataObj + '},';
+                } else {
+                    string += member + ': ' + String(script[member]) + ',';
+                }
                 break;
             case 'string':
                 string += member + ': ' + JSON.stringify(script[member]) + ',';
                 break;
             case 'object':
                 if (member === 'data') {
-                    string += member + ': ' + JSON.stringify(script[member]) + ',';
+                    string += member + ': ' + (0, _xss2.default)(JSON.stringify(script[member])) + ',';
                 } else {
                     string += member + ': ' + scriptToString(script[member]) + ',';
                 }
