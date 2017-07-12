9bf392fa2dca39630edd27477a78f1802ce5fc10
Daniel Cherubini <daniel@cherubini.casa>
2017-07-12 22:09:51 +0200

    cleaned up .. tests are broken

--- test/models/models.js
+++ test/models/models.js
@@ -1,80 +1,56 @@
 import test   from 'ava';
-import {Defaults, Types, DataObject} from '../../lib/models';
+import path from 'path';
+import {Defaults, Types, DataObject} from '../../src/models';
 
-const defaults = {
-    layout: 'fooBar',
-    settings: {
-        vue: {
-            componentsDir: '/baz',
-            defaultLayout: 'qux'
-        },
-        views: '/foo/bar'
+const options = {
+    rootPath: path.join(__dirname, 'tests'),
+    componentsPath: 'vueFiles/components',
+    viewsPath: 'vueFiles',
+    layout: {
+        start: '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"><script src="https://unpkg.com/vue/dist/vue.js"></script>',
+        middle: '<body><div id="app">',
+        end: '</div></body></html>'
     }
 };
 const types         = new Types();
-const defaultObject = new Defaults(defaults);
-const dataObject    = new DataObject(defaults, defaultObject, types.COMPONENT);
-const dataObjectSub = new DataObject(defaults, defaultObject, types.SUBCOMPONENT);
-const emptyObject   = new Defaults({settings: {views:'/foo/bar'}});
+const defaultObject = new Defaults(options);
+const dataObject    = new DataObject(options, defaultObject, types.COMPONENT);
+const dataObjectSub = new DataObject(options, defaultObject, types.SUBCOMPONENT);
 
 //Examples
 const exampleObject = {
-  rootPath: '/foo/bar/',
-  layoutsDir: '',
-  componentsDir: '/baz/',
-  customLayout: '/foo/bar/fooBar',
-  defaultLayout: '/foo/bar/qux',
-  options: {
-      settings: {
-          vue: {
-              componentsDir: '/baz',
-              defaultLayout: 'qux'
-          },
-          views: '/foo/bar'
-      }
-  },
-  backupLayout: '<template><!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"><script src="https://unpkg.com/vue/dist/vue.js"></script></head><body>{{{app}}}{{{script}}}</body></html></template><script></script><style></style>',
-  layoutPath: '/foo/bar/qux.vue'
+    rootPath: options.rootPath,
+    componentsPath: options.rootPath + '/' + options.componentsPath,
+    viewsPath:  options.rootPath + '/' + options.viewsPath,
+    layout: {
+        start: '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"><script src="https://unpkg.com/vue/dist/vue.js"></script>',
+        middle: '<body><div id="app">',
+        end: '</div></body></html>',
+    },
+    options:{
+        rootPath: options.rootPath,
+        componentsPath: 'vueFiles/components',
+        viewsPath: 'vueFiles'
+    },
+    cache: {
+        max: 500,
+        maxAge: 3600000
+    }
 };
-
-test('Components Directory', t => {
-    t.is(defaultObject.componentsDir, exampleObject.componentsDir);
-});
 
 test('Root Path', t => {
     t.is(defaultObject.rootPath, exampleObject.rootPath);
 });
 
-test('Backup Layout', t => {
-    t.is(defaultObject.backupLayout, exampleObject.backupLayout);
+test('Components Path', t => {
+    t.is(defaultObject.componentsPath, exampleObject.componentsPath);
 });
+
+test('Views Path', t => {
+    t.is(defaultObject.viewsPath, exampleObject.viewsPath);
+});
+
 
 test('Default Layout', t => {
-    t.is(defaultObject.defaultLayout, exampleObject.defaultLayout);
-});
-
-test('layoutsDir', t => {
-    t.is(defaultObject.layoutsDir, exampleObject.layoutsDir);
-});
-
-test('customLayout', t => {
-    t.is(defaultObject.customLayout, exampleObject.customLayout);
-});
-
-
-//Empty
-test('empty rootPath', t => {
-    t.is(emptyObject.rootPath, '/foo/bar/');
-});
-test('empty layoutsDir', t => {
-    t.is(emptyObject.layoutsDir, '');
-});
-test('empty componentsDir', t => {
-    t.is(emptyObject.componentsDir, '');
-});
-test('empty defaultLayout', t => {
-    t.is(emptyObject.defaultLayout, '');
-});
-test('empty options', t => {
-    t.is(emptyObject.options.settings.views, '/foo/bar');
+    t.deepEqual(defaultObject.layout, exampleObject.layout);
 });
