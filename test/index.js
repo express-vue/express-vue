22f061ebd6387d5b34ff66f1e78f27bc22aaef8a
Daniel Cherubini <daniel@cherubini.casa>
2017-08-04 10:09:02 +0200

    updated tests

--- test/index.js
+++ test/index.js
@@ -33,14 +33,15 @@ const vue = {
     }
 };
 
-const exampleResponse = `<!DOCTYPE html><html><head><title>pageTitle</title>
+const exampleResponse = `<!DOCTYPE html><html><head>
+<title>pageTitle</title>
 <meta property="og:title" content="pageTitle"/>
 <meta name="twitter:title" content="pageTitle"/>
-</head><body><div id="app"><div data-server-rendered="true"><h1>pageTitle</h1><p>Welcome to the pageTitle demo. Click a link:</p><button type="button" name="button">Say FOO</button> <input placeholder="edit me" value="Hello!"><div><h1>Hello!</h1></div><div><ul></ul></div></div><script>
+</head><body><div id="app"><div data-server-rendered="true"><h1>pageTitle</h1><p>Welcome to the pageTitle demo. Click a link:</p><button type="button" name="button">Say FOO</button> <input placeholder="edit me" value="Hello!"><div><h1>Hello!</h1></div><div><ul></ul></div></div></div><script>
 (function () {'use strict';var createApp = function () {return new Vue({mixins: [{methods: {hello: function () {
             console.log('Hello');
         },},},],data: function(){return {"title":"pageTitle","message":"Hello!","users":[]}},components: {messageComp: {props: ["message"],template: "<div class=\\"\\"><h1>{{message}}</h1></div>",},users: {props: ["users","title"],template: "<div class=\\"\\"><ul><li v-for=\\"user in users\\"><a v-bind:href=\\"'/users/' + user.name\\" class=\\"test\\">{{ user.name }}</a></li></ul></div>",},},template: "<div><h1>{{title}}</h1><p>Welcome to the {{title}} demo. Click a link:</p><button type=\\"button\\" name=\\"button\\" v-on:click=\\"hello\\">Say FOO</button> <input v-model=\\"message\\" placeholder=\\"edit me\\"><message-comp :message=\\"message\\"></message-comp><users :users=\\"users\\"></users></div>",})};if (typeof module !== 'undefined' && module.exports) {module.exports = createApp} else {this.app = createApp()}}).call(this);app.$mount('#app');
-</script></div></body>`
+</script></body></html>`
 
 const exampleError = `Could not find view file at ${vueOptions.rootPath}/indexfake.vue`;
 
