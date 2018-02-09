a84c8a836b308446b2a11f996a0c85183965b1e6
Daniel Cherubini <daniel@cherubini.casa>
2018-02-09 15:27:42 +0100

    getting ready for release

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,16 @@
-declare function init(options: Object): Function;
 
-export = init;
+interface VueOptionsType {
+    title?: String,
+    head?: {
+        scripts: Object[],
+        metas: Object[],
+        styles: Object[]
+    }
+    template?: Object
+}
+
+declare function init(options: VueOptionsType): Function;
+
+export {
+    init
+};
--- test/example/index.js
+++ test/example/index.js
@@ -1,92 +1,84 @@
-const path = require('path');
-const express = require('express');
-const expressVue = require('../../src');
+const path = require("path");
+const express = require("express");
+const expressVue = require("../../lib");
 const app = express();
 
 const vueOptions = {
-    rootPath: path.join(__dirname, '/views'),
-    layout: {
-        start: '<body><div id="app">',
-        end: '</div></body>'
-    },
-    head: {
-        scripts: [
-            { src: 'https://unpkg.com/vue@2.4.2/dist/vue.js' }
-        ]
-    }
+    rootPath: path.join(__dirname, "/views"),
 };
 const expressVueMiddleware = expressVue.init(vueOptions);
 app.use(expressVueMiddleware);
 
 var users = [];
-var pageTitle = 'Express Vue';
+var pageTitle = "Express Vue";
 users.push({
-    name: 'tobi',
-    age: 12
+    name: "tobi",
+    age: 12,
 });
 users.push({
-    name: 'loki',
-    age: 14
+    name: "loki",
+    age: 14,
 });
 users.push({
-    name: 'jane',
-    age: 16
+    name: "jane",
+    age: 16,
 });
 
 var exampleMixin = {
     methods: {
-        hello: function () {
-            console.log('Hello');
-        }
-    }
+        hello: function() {
+            // tslint:disable-next-line:no-console
+            console.log("Hello");
+        },
+    },
 };
 
-app.get('/', function (req, res) {
+app.get("/", function(req, res) {
     const data = {
         title: pageTitle,
-        message: 'Hello!',
-        users: users
+        message: "Hello!",
+        users: users,
     };
 
     const vue = {
         head: {
             title: pageTitle,
             metas: [{
-                property: 'og:title',
-                content: pageTitle
+                property: "og:title",
+                content: pageTitle,
             },
             {
-                name: 'twitter:title',
-                content: pageTitle
+                name: "twitter:title",
+                content: pageTitle,
             }, {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
-            }
+                name: "viewport",
+                content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+            },
             ],
             structuredData: {
-                '@context': 'http://schema.org',
-                '@type': 'Organization',
-                'url': 'http://www.your-company-site.com',
-                'contactPoint': [{
-                    '@type': 'ContactPoint',
-                    'telephone': '+1-401-555-1212',
-                    'contactType': 'customer service'
-                }]
-            }
-        }
+                "@context": "http://schema.org",
+                "@type": "Organization",
+                "url": "http://www.your-company-site.com",
+                "contactPoint": [{
+                    "@type": "ContactPoint",
+                    "telephone": "+1-401-555-1212",
+                    "contactType": "customer service",
+                }],
+            },
+        },
     };
-    res.renderVue('index.vue', data, vue);
+    res.renderVue("index.vue", data, vue);
 });
 
-app.get('/users/:userName', function (req, res) {
-    var user = users.filter(function (item) {
+app.get("/users/:userName", function(req, res) {
+    var user = users.filter(function(item) {
         return item.name === req.params.userName;
     })[0];
-    res.renderVue('user.vue', {
-        title: 'Hello My Name is',
-        user: user
+    res.renderVue("user.vue", {
+        title: "Hello My Name is",
+        user: user,
     });
 });
 
 app.listen(3000);
-console.log('Express server listening on port 3000');
+console.log("Express server listening on port 3000");
