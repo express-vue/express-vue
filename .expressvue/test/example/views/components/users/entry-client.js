dd890d6f89789f41723b26c97642228d4e0bbab8
Daniel Cherubini <daniel@cherubini.casa>
2019-01-30 15:57:12 +0100

    updating vue-pronto and fixing audit

--- .expressvue/test/example/views/components/users/entry-client.js
+++
@@ -1,4 +0,0 @@
-import { createApp } from "./app"
-        const store = window.__INITIAL_STATE__;
-        const { app } = createApp(store ? store : {});
-        app.$mount("#app")
--- .expressvue/test/example/views/components/users/entry-server.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "./app"
-        export default context => {
-            return new Promise((resolve, reject) => {
-                const { app } = createApp(context);
-
-                resolve(app);
-            });
-        };
